a1de76932dbcaea7e0296ca52953c1b493f7facc
System Administrator <root@Qihaos-MBP.hsd1.ca.comcast.net>
2019-08-03 17:23:34 -0700

    first commit, blocked by nba for fetching player data

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TopNavBar from './TopNavBar';
+import { Main } from './Main';
+
+function App() {
+    return (
+        <div className="App">
+            <TopNavBar/>
+            <Main/>
+            this is app
+        </div>
+    );
+}
+
+export default App;
