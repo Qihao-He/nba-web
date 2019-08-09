79ca6f20adbb2b651957d1b9fe8d68a75f6f6a90
Qihao He <qi.he@maine.edu>
2019-08-08 19:23:18 -0700

    searchbar add ant design

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
 
 class SearchBar extends Component {
     render() {
         return (
-            <div>
-
+            <div className="global-search-wrapper" style={{ width: 300 }}>
+                <AutoComplete
+                    className="global-search"
+                    size="large"
+                    style={{ width: '100%' }}
+                    // dataSource={dataSource.map(renderOption)}
+                    // onSelect={onSelect}
+                    // onSearch={this.handleSearch}
+                    placeholder="input here"
+                    optionLabelProp="text"
+                >
+                    <Input
+                        suffix={
+                            <Button
+                                className="search-btn"
+                                style={{ marginRight: -12 }}
+                                size="large"
+                                type="primary"
+                            >
+                                <Icon type="search" />
+                            </Button>
+                        }
+                    />
+                </AutoComplete>
             </div>
         );
     }
