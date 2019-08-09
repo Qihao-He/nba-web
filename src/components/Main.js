4d866a0ed34586a8c050aed548ca618c1cb0bc21
Qihao He <qi.he@maine.edu>
2019-08-08 19:16:14 -0700

    add SearchBar js css into main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Profile } from './Profile';
 import nba from 'nba';
 import {DataViewContainer} from './DataViewContainer';
+import SearchBar from './SearchBar';
 
 export class Main extends React.Component {
     state = {
@@ -24,8 +25,11 @@ export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Profile playerInfo={this.state.playerInfo} />
-                <DataViewContainer playerId={this.state.playerId} />
+                <SearchBar />
+                <div classname="player">
+                    <Profile playerInfo={this.state.playerInfo} />
+                    <DataViewContainer playerId={this.state.playerId} />
+                </div>
             </div>
         );
     }
